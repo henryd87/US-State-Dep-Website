19e3f5ac77b1fb68dcc11348dbcf3ed19ce86d29
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-01 00:30:39 +0000

    started working on the universal top bar and created the image folder. We are only working on the top white bar that will be displayed on every page.

--- statedepwebsite/src/App.js
+++ statedepwebsite/src/App.js
@@ -1,10 +1,10 @@
 import logo from './logo.svg';
 import './App.css';
-import "./top3HeadBars"
+
 function App() {
   return (
     <div className="App">
-      
+      working?
     </div>
   );
 }
