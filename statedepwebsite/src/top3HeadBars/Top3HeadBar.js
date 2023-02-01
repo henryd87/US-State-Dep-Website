a9aafdbdadcd24f5b75e728d19a9aa77afbda600
henryd87 <98516069+henryd87@users.noreply.github.com>
2023-02-01 05:20:32 +0000

    started doing the top navy bars, only have initially created them; no actual buttons.

--- statedepwebsite/src/top3HeadBars/Top3HeadBar.js
+++ statedepwebsite/src/top3HeadBars/Top3HeadBar.js
@@ -1,10 +1,12 @@
 import React from "react"
 import UniversalWhiteTopBar from "./UniversalWhiteTopBar"
+import TopDarkNavyBars from "./TopDarkNavyBars"
 export default function top3HeadBar(){
     return(
         <div>
             <UniversalWhiteTopBar/>
-
+            
+            <TopDarkNavyBars/>
         </div>
     )
 }
---
+++ statedepwebsite/src/top3HeadBars/TopDarkNavyBars.js
@@ -0,0 +1,13 @@
+import React from "react"
+import "./TopDarkNavyBars.css"
+export default function TopDarkNavyBars(){
+    return (
+        <div className="overallTopNavyBar">
+            <div className="topNavy">
+            </div>
+            <div className="bottomNavy">
+            </div>
+        </div>
+
+    )
+}
